b280467c0a9f9d7ea0a4b453a55ae6b86ede633a
Adv <68746946+Adv13@users.noreply.github.com>
2023-09-20 15:06:56 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SliderM from "./components/Slider/SliderM"
+import SliderA from "./components/Slider/SliderA"
+
+
+function App() {
+
+  return (
+    <>
+      return <SliderA/>
+    </>
+  )
+}
+
+export default App
